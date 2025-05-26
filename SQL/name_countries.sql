21533d4609eeb607d690ee1bcd777a64c5aa2084
juls-byte <juliav333@gmail.com>
2025-05-26 14:10:28 +0500

    Delete SQL/name_countries.sql

--- SQL/name_countries.sql
+++
@@ -1,7 +0,0 @@
--- Выведите название страны, где находится город «Salzburg»
-SELECT Countries.name AS country_name
-FROM Countries JOIN Regions
-ON Countries.id=Regions.countryid
-JOIN Cities
-ON Regions.id=Cities.regionid
-WHERE Cities.name = 'Salzburg'
